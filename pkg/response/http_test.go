27030e67c23a106f225048026fb134e6ef8b4898
dimkouv <dimkouv@protonmail.com>
2020-02-16 00:11:53 +0200

    Write tests for response module

---
+++ pkg/response/http_test.go
@@ -0,0 +1,57 @@
+package response
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTTP(t *testing.T) {
+	w := &httptest.ResponseRecorder{
+		Code:    0,
+		Body:    nil,
+		Flushed: false,
+	}
+
+	resp := HTTP(w)
+	assert.Equal(t, w, resp.w)
+	assert.Empty(t, resp.data)
+}
+
+func TestResponse_Data(t *testing.T) {
+	w := &httptest.ResponseRecorder{}
+	b := []byte("hello")
+	resp := HTTP(w).Data(b)
+	assert.Equal(t, resp.data, b)
+}
+
+func TestResponse_Status(t *testing.T) {
+	w := &httptest.ResponseRecorder{}
+	resp := HTTP(w).Status(http.StatusBadRequest)
+	assert.Equal(t, resp.status, http.StatusBadRequest)
+}
+
+func TestResponse_JSON(t *testing.T) {
+	w := &httptest.ResponseRecorder{}
+	resp := HTTP(w)
+	resp.JSON()
+	assert.Equal(t, resp.w.Header().Get("content-type"), "application/json")
+}
+
+func TestResponse_TEXT(t *testing.T) {
+	w := &httptest.ResponseRecorder{}
+	resp := HTTP(w)
+	resp.TEXT()
+	assert.Equal(t, resp.w.Header().Get("content-type"), "text/plain")
+}
+
+func TestResponse_Error(t *testing.T) {
+	w := &httptest.ResponseRecorder{}
+	err := errors.New("just an error")
+	resp := HTTP(w).Data([]byte("hello")).Error(err).Data([]byte("hello"))
+	resp.JSON()
+	assert.Equal(t, `{"msg":"`+err.Error()+`"}`, string(resp.data))
+}
