fffea20867c4d4ecf3c33a70c102bf25c4330c1d
dimkouv <dimkouv@protonmail.com>
2020-02-15 14:47:21 +0200

    Add some tests for tracking service

---
+++ internal/services/tracking_test.go
@@ -0,0 +1,43 @@
+package services
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/dimkouv/trackpal/internal/models"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dimkouv/trackpal/internal/repository"
+)
+
+func TestTrackingService_GetDevicesAsJSON(t *testing.T) {
+	repo := repository.NewTrackingRepositoryMock()
+	server := NewTrackingService(repo)
+	devices := make([]models.Device, 0)
+
+	t.Run("if no devices exist a null response is received", func(t *testing.T) {
+		b, err := server.GetDevicesAsJSON()
+		assert.NoError(t, err)
+		err = json.Unmarshal(b, &devices)
+		assert.NoError(t, err)
+		assert.Empty(t, devices)
+	})
+
+	t.Run("if devices exists they should be received", func(t *testing.T) {
+		createdDevices := []models.Device{
+			{Name: "dev1"}, {Name: "dev2"}, {Name: "dev3"}, {Name: "dev4"},
+		}
+		for _, device := range createdDevices {
+			_, err := repo.SaveNewDevice(device)
+			assert.NoError(t, err)
+		}
+
+		b, err := server.GetDevicesAsJSON()
+		err = json.Unmarshal(b, &devices)
+		assert.NoError(t, err)
+		for i := range createdDevices {
+			assert.Equal(t, createdDevices[i].Name, devices[i].Name)
+		}
+	})
+}
